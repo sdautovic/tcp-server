3afc9c7a5ff3d41a0012ebd18703f01643dae130
Sasa Dautovic <Sasa.Dautovic@optima-telekom.hr>
2016-05-16 14:48:35 +0200

    Periodic commit

--- src/test/java/hr/sdautovic/net/tcp/server/PingPongTcpServerApplication.java
+++ src/test/java/hr/sdautovic/net/tcp/server/PingPongTcpServerApplication.java
@@ -35,9 +35,7 @@ public class PingPongTcpServerApplication {
 				};
 			}
 		});
-//		ServerSocket serverSocket = new ServerSocket(9000, 0, InetAddress.getByName("0.0.0.0"));
-		
-		ServerThread serverThread = new ServerThread(server, "0.0.0.0", 9000, 1);
+		ServerThread serverThread = new ServerThread(server, "0.0.0.0", 9000, 10);
 		serverThread.start();
 	}
 }
